c89e18de604b2c611185c22be272dedd3632c5c2
Denis Kravchuk <deniskrv@gmail.com>
2022-05-18 19:57:17 +0300

    add delete function to card

--- models/card.js
+++ models/card.js
@@ -2,7 +2,7 @@ const path = require('path')
 const fs = require('fs')
 
 const p = path.join(
-   path.dirname(process.mainModule.filename),
+    path.dirname(process.mainModule.filename),
     'data',
     'card.json'
 )
@@ -41,6 +41,31 @@ class Card {
                     reject(err)
                 } else {
                     resolve(JSON.parse(content))
+                }
+            })
+        })
+    }
+
+    static async remove(id) {
+        const card = await Card.fetch()
+
+        const idx = card.courses.findIndex(c => c.id === id)
+        const course = card.courses[idx]
+
+        if (course.count === 1) {
+            card.courses = card.courses.filter(c => c.id !== id)
+        } else {
+            card.courses[idx].count--
+        }
+
+        card.price -= course.price
+
+        return new Promise((resolve, reject) => {
+            fs.writeFile(p, JSON.stringify(card), err => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(card)
                 }
             })
         })
