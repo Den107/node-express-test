c83e550c9a06e1ece15f55bbe0d728c5f9d831d5
Denis Kravchuk <deniskrv@gmail.com>
2022-05-18 19:18:11 +0300

    make card model

--- routes/card.js
+++ routes/card.js
@@ -4,16 +4,18 @@ const Course = require('../models/course')
 const router = Router()
 
 router.post('/add', async (req, res) => {
-const course = await Course.getById(req.body.id)
+    const course = await Course.getById(req.body.id)
     await Card.add(course)
     res.redirect('/card')
 })
 
-router.get('/', async (req, res)=>{
+router.get('/', async (req, res) => {
     const card = await Card.fetch()
-    res.render('card',{
+    res.render('card', {
         title: 'Card',
-        card
+        isCard: true,
+        courses: card.courses,
+        price: card.price
     })
 })
 
