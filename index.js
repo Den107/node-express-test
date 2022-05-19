9ea90ff6f5796afdc026a44934915f626584e371
Denis Kravchuk <deniskrv@gmail.com>
2022-05-19 16:22:31 +0300

    add user and add user to courses

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const path = require("path");
 const mongoose = require('mongoose')
 const Handlebars = require('handlebars')
 const exphb = require('express-handlebars')
-const  { allowInsecurePrototypeAccess }  =  require ( '@handlebars/allow-prototype-access' )
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const homeRoutes = require('./routes/home.js')
 const coursesRoutes = require('./routes/courses.js')
 const addRoutes = require('./routes/add.js')
 const cardRoutes = require('./routes/card.js')
+const User = require('./models/user.js')
 
 
 const app = express()
@@ -22,6 +23,15 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(async (req, res, next)=>{
+    try{
+        req.user = await User.findById('6286342aa1f019e3d05d3bfb')
+        next()
+    }catch (e) {
+        console.log(e)
+    }
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
@@ -33,8 +43,17 @@ app.use('/card', cardRoutes)
 const PORT = process.env.PORT || 3000
 
 async function start() {
-    await mongoose.connect('mongodb://localhost:27017/shop')
     try {
+        await mongoose.connect('mongodb://localhost:27017/shop')
+        const candidate = await User.findOne()
+        if (!candidate) {
+            const user = new User({
+                email: 'den@mail.com',
+                name: 'Den',
+                cart: {items: []}
+            })
+            await user.save()
+        }
         app.listen(PORT, () => {
             console.log(`Server is start on port ${PORT}`)
         })
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,6 @@
 const {Router} = require('express')
 const Course = require('../models/course')
+const {SchemaType} = require("mongoose");
 const router = Router()
 
 router.get('/', (req, res) => {
@@ -13,7 +14,8 @@ router.post('/', async (req, res) => {
    const course = new Course({
        title: req.body.title,
        price: req.body.price,
-       img: req.body.img
+       img: req.body.img,
+       userId: req.user._id
    })
     try{
         await course.save()
