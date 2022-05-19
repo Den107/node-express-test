2b8d0ca0e82cc5a56a6a389127a6ee48037c7c26
Denis Kravchuk <deniskrv@gmail.com>
2022-05-20 01:03:58 +0300

    add function to add course on cart

--- routes/card.js
+++ routes/card.js
@@ -1,27 +1,27 @@
 const {Router} = require('express')
-const Card = require('../models/card')
 const Course = require('../models/course')
 const router = Router()
 
 router.post('/add', async (req, res) => {
-    const course = await Course.getById(req.body.id)
-    await Card.add(course)
+    const course = await Course.findById(req.body.id)
+    await req.user.addToCart(course)
     res.redirect('/card')
 })
 
-router.delete('/remove/:id', async (req, res)=>{
-    const card= await Card.remove(req.params.id)
+router.delete('/remove/:id', async (req, res) => {
+    const card = await Card.remove(req.params.id)
     res.status(200).json(card)
 })
 
 router.get('/', async (req, res) => {
-    const card = await Card.fetch()
-    res.render('card', {
-        title: 'Card',
-        isCard: true,
-        courses: card.courses,
-        price: card.price
-    })
+    // const card = await Card.fetch()
+    // res.render('card', {
+    //     title: 'Card',
+    //     isCard: true,
+    //     courses: card.courses,
+    //     price: card.price
+    // })
+    res.json({test:true})
 })
 
 module.exports = router
